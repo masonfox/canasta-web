5f05d2761444907e4d9c0b31cd4cdc548193ec5a
Mason Fox <masonfox22@gmail.com>
2020-08-23 15:07:32 -0400

    basic nav

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app">
-    <LogsView />
-    <TrackerView />
+    <NavBar />
+    <div id="app-wrapper">
+      <LogsView />
+      <TrackerView />
+    </div>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar'
 import LogsView from '@/views/LogsView'
 import TrackerView from '@/views/TrackerView'
 
 export default {
   name: 'App',
-  components: { LogsView, TrackerView }
+  components: { LogsView, TrackerView, NavBar }
 }
 </script>
 
 <style lang="scss">
-  #app {
+  #app-wrapper {
     padding: 2.5rem 0;
   }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <b-navbar variant="dark" type="dark">
+        <b-navbar-brand tag="h1" class="mb-0">Canasta Calculator</b-navbar-brand>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style>
+
+</style>
