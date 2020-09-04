1f68e12c6402329188ec56aec71846b9ef395176
Mason Fox <mfox@sscorp.com>
2020-09-04 09:20:39 -0400

    resolves #11, don't calculate round score on fly

--- src/components/RoundStatsRow.vue
+++ src/components/RoundStatsRow.vue
@@ -1,12 +1,26 @@
 <template>
-    <td :class="classObject">{{ roundScore.total }}</td>
+    <td :class="classObject">{{ score }}</td>
 </template>
 
 <script>
 export default {
     name: 'RoundStatsRow',
     props: ['round', 'team'],
+    data () {
+        return {
+            defaultVal: "-"
+        }
+    },
     computed: {
+        isActiveRound () {
+            return this.round.id == this.currentRoundId
+        },
+        score () {
+            return (this.isActiveRound) ? this.defaultVal : this.roundScore.total
+        },
+        currentRoundId () {
+            return this.$store.getters.currentRound.id
+        },
         roundScore () {
             return this.$store.getters.getRoundResultsByTeam(this.round.id, this.team.id)
         },
@@ -16,8 +30,9 @@ export default {
         classObject () {
             return {
                 'font-weight-bold': true,
-                'text-success': this.won,
-                'text-danger': !this.won
+                'text-success': this.won && !this.isActiveRound,
+                'text-danger': !this.won && !this.isActiveRound,
+                'text-dark': this.isActiveRound
             }
         }
     }
