1c7cd508bf30cdb86635017faaa1bd53a33edceb
Mason Fox <masonfox22@gmail.com>
2020-09-05 23:45:07 -0400

    better display logic for game stats

--- src/components/GameStatsItem.vue
+++ src/components/GameStatsItem.vue
@@ -2,7 +2,7 @@
     <b-col sm="6" class="game-stats-container">
         <div class="game-stats-item">
             <h5 class="title">{{ team.name }}</h5>
-            <p :class="classObject">{{ score | formatNumber }}</p>
+            <p :class="classObject">{{ scoreDisplay }}</p>
         </div>
     </b-col>
 </template>
@@ -20,15 +20,21 @@ export default {
         score () {
             return this.$store.getters.getGameResultsByTeam(this.team.id)
         },
+        scoreDisplay () {
+            return (this.score > 0) ? this.$options.filters.formatNumber(this.score) : "-"
+        },
+        isRoundOne () {
+            return this.$store.state.game.currentRound == 1
+        },
         isWinningGame () {
-            return this.$store.getters.isWinningGame(this.team.id)
+            return (!this.isRoundOne) ? this.$store.getters.isWinningGame(this.team.id) : false
         },
         classObject () {
             return {
                 'score': true,
                 'text-success': this.isWinningGame,
                 'text-danger': !this.isWinningGame,
-                'text-dark': false
+                'text-dark': this.isRoundOne
             }
         }
     }
