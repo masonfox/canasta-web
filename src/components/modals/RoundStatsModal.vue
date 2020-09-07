54b206021cefd008cfd8644b53c195da5b350e93
Mason Fox <mfox@sscorp.com>
2020-09-06 20:39:11 -0400

    #3, functional round subtotal detail

--- src/components/modals/RoundStatsModal.vue
+++ src/components/modals/RoundStatsModal.vue
@@ -1,29 +1,82 @@
 <template>
-  <b-modal size="xl" id="round-stats" header-bg-variant="light" title="RoundStats">
-    <table class="table stats-table table-hover">
+  <b-modal size="xl" id="round-stats" header-bg-variant="light" title="Round Detail">
+    <div class="text-center">
+        <h6>Select Round</h6>
+        <div class="d-flex mb-3 justify-content-center align-items-center">
+            <b-button size="sm" variant="dark" @click="decrement" :disabled="decrementDisabled">
+            <b-icon icon="dash" />
+            </b-button>
+            <b-badge class="mb-0 mx-2" style="font-size: 13px; font-weight: 500; padding: .45rem;" variant="primary">Round {{ roundId }}</b-badge>
+            <b-button size="sm" variant="dark" @click="increment" :disabled="incrementDisabled">
+                <b-icon icon="plus" />
+            </b-button>
+        </div>
+    </div>
+    <table class="table table-bordered table-hover">
         <thead>
             <tr>
-                <th scope="col">Team</th>
-                <th scope="col">Canasta</th>
-                <th scope="col">Loose</th>
-                <th scope="col">Remaining</th>
-                <th scope="col">Total</th>
+                <th width="20%" scope="col">Team</th>
+                <th width="12%" scope="col">Canasta</th>
+                <th width="12%" scope="col">Loose</th>
+                <th width="12%" scope="col">Remaining</th>
+                <th width="12%" scope="col">Out Bonus</th>
+                <th width="10%" scope="col">Total</th>
             </tr>
         </thead>
         <tbody>
-            <h1>Hello</h1>
+            <RoundStatsModalRow v-for="team in teams" :roundId="roundId" :team="team" :key="team.id" />
         </tbody>
     </table>
   </b-modal>
 </template>
 
 <script>
+import RoundStatsModalRow from './components/RoundStatsModalRow'
+
 export default {
     name: 'RoundStatsModal',
-
+    components: {
+        RoundStatsModalRow
+    },
+    data () {
+        return {
+            roundId: 1
+        }
+    },
+    computed: {
+        teams () {
+            return this.$store.state.teams
+        },
+        currentRoundId () {
+            return this.$store.state.game.currentRound
+        },
+        decrementDisabled () {
+            return this.roundId == 1
+        },
+        incrementDisabled () {
+            if (this.$store.state.game.ended) {
+                return this.roundId == this.currentRoundId
+            } else {
+                return this.roundId == (this.currentRoundId - 1)
+            }
+        }
+    },
+    methods: {
+        increment () {
+            this.roundId++
+        },
+        decrement () {
+            this.roundId--
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
+#round-stats {
 
+    tbody th {
+        border-right: 1px solid #ddd;
+    }
+}
 </style>
---
+++ src/components/modals/components/RoundStatsModalRow.vue
@@ -0,0 +1,35 @@
+<template>
+    <tr>
+        <th>{{ team.name }}</th>
+        <td>{{ results.canasta | formatNumber }}</td>
+        <td>{{ results.loose | formatNumber }}</td>
+        <td>{{ results.remaining | formatNumber }}</td>
+        <td>{{ results.wentOutBonus }}</td>
+        <td><b>{{ results.total | formatNumber }}</b></td>
+    </tr>
+</template>
+
+<script>
+export default {
+    name: 'RoundStatsModalRow',
+    props: {
+        team: {
+            type: Object,
+            required: true
+        },
+        roundId: {
+            type: Number,
+            required: true
+        }
+    },
+    computed: {
+        results () {
+            return this.$store.getters.getRoundResultsByTeam(this.roundId, this.team.id)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
